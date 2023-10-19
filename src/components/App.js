9e057d56bbe4cddbd74b54ca853905893e44733d
Mani Poorna <ndmprao2001@gmail.com>
2023-10-19 11:02:44 +0530

    V 1.0

--- src/components/App.js
+++ src/components/App.js
@@ -1,2 +1,62 @@
-<p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
+import React, { useState } from 'react'
 
+const App = () => {
+  const users = [
+    { id: 1, name: "ABC", email: "abc@gmail.com", password: "12" },
+    { id: 2, name: "DEF", email: "def@gmail.com", password: "1234" },
+    { id: 3, name: "GHI", email: "ghi@gmail.com", password: "123456" }
+  ]
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+  
+  const [Uerror, setUError] = useState(false)
+  const [errUMsg, setUErrMsg] = useState("");
+
+  const [errMsg, setErrMsg] = useState("");
+  const [Perror, setPError] = useState(false)
+
+  function checkUser(e) {
+    e.preventDefault();
+    // console.log(email, password)
+    if (email !== "" && password !== "") {
+      const checkEmailExists = users.find(user => user.email === email);
+      if (checkEmailExists) {
+        if (password !== checkEmailExists.password) {
+          setPError(true)
+          setUErrMsg(false)
+          setErrMsg("Password Incorrect")
+        }
+        else {
+          setTimeout(() => {
+            console.log(checkEmailExists)
+          }, 3000);
+          setPError(false)
+          setUErrMsg(false)
+        }
+      }
+      else {
+        setUError(true)
+        setPError(false)
+        setUErrMsg("User not found")
+      }
+    }
+  }
+
+  return (
+    <div>
+      <form onSubmit={(e) => checkUser(e)}>
+        <input id='input-email' type="email" placeholder="Email" onChange={(e) => setEmail(e.target.value)} /><br />
+        <input id='input-password' type="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)} /><br />
+        <button type='submit' id='submit-form-btn' >Submit</button>
+        {
+          Uerror ? <p id='user-error' style={{ color: "red" }}>{errUMsg}</p> : ""
+        }
+        {
+          Perror ? <p id='password-error' style={{ color: "red" }}>{errMsg}</p> : ""
+        }
+      </form>
+    </div>
+  )
+}
+
+export default App
